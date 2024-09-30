ae76d28efe426b371afb518334d738248340d10c
ismail ibrahim <ismailibrahimabiodun@gmail.com>
2024-09-30 04:31:32 -0700

    updating

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -1,24 +1,138 @@
-import React from 'react'
-import body from '../../assets/goto.jpg';
+import React from "react";
+import body from "../../assets/goto.jpg";
 
 const Home = () => {
   return (
     <>
-     <div className='container-fluid'>
+      <div className="container-fluid">
         <div>
-          <h1 className='phone'>Welcome to Hakanni High School</h1>
+          <h1 className="phone">Welcome to Hakanni High School</h1>
         </div>
-        <img src={body} alt="Back" className='photo'/>
+        <img src={body} alt="Back" className="photo" />
       </div>
 
-      <div className='flex2'>
-       <div >My name z zico</div>
-       <div> my name z mike</div>
-       <div>my name z ayo</div>
-        
-      </div>
+      
+
+      <section
+        className="upcoming-event text-center text-light py-5 mt-5"
+        style={{backgroundColor: 'grey'}}
+        >
+        <div className="container my-4">
+          <form
+            className="d-flex justify-content-center align-items-center mt-3"
+            action="#"
+            method="POST"
+          >
+            <input
+              type="email"
+              className="form-control w-50 me-2"
+              placeholder="Email address"
+              required
+            />
+            <button type="submit" className="btn btn-light">
+              SUBSCRIBE
+            </button>
+          </form>
+          <hr className="bg-white my-4" />
+          <div className="row">
+            <div className="col-md-3">
+              <h6>She Leads Africa</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    About Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Contact Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Careers
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <h6>Resources</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Advice
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Guild
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    SLAY Festival
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3">
+              <h6>Discover</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    MMI
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    SMB
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    HGCP
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Contribute
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="col-md-3 mt-3">
+              <h6>Support</h6>
+              <ul className="list-unstyled">
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Press
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Partner & Advertise With Us
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="text-white text-decoration-none">
+                    Privacy Policy
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <p className="mt-4">
+            &copy; 2023 SHE LEADS AFRICA, All Rights Reserved.
+          </p>
+        </div>
+      </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Component/Navbar/Nav.jsx
+++ src/Component/Navbar/Nav.jsx
@@ -20,49 +20,56 @@ const Nav = () => {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav justify-content-between mx-auto">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-              </li>
+            <div className="mx-auto">
+              <ul className="navbar-nav justify-content-between">
+                <li className="nav-item mx-2">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                </li>
 
-              <li className="nav-item dropdown">
-                <a
-                  className="nav-link dropdown-toggle"
-                  data-bs-toggle="dropdown"
-                  href="#"
-                  role="button"
-                  aria-expanded="false"
-                >
-                  Account
-                </a>
-                <ul className="dropdown-menu">
-                  <li className="nav-item">
-                    <Link to="/Signup" id="acct"> STUDENT </Link>
-                  </li>
+                <li className="nav-item dropdown mx-4">
+                  <a
+                    className="nav-link dropdown-toggle"
+                    data-bs-toggle="dropdown"
+                    href="#"
+                    role="button"
+                    aria-expanded="false"
+                  >
+                    Account
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li className="nav-item">
+                      <Link to="/Signup" id="acct">
+                        {" "}
+                        STUDENT{" "}
+                      </Link>
+                    </li>
 
-                  <li className="nav-item">
-                    <Link to="/AdminSignin" id="acct">STAFF</Link>
-                  </li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/About">
-                  About
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/Course">
-                  Course
-                </Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/Faq">
-                  FAQ
-                </Link>
-              </li>
-            </ul>
+                    <li className="nav-item">
+                      <Link to="/AdminSignin" id="acct">
+                        STAFF
+                      </Link>
+                    </li>
+                  </ul>
+                </li>
+                <li className="nav-item mx-4">
+                  <Link className="nav-link" to="/About">
+                    About
+                  </Link>
+                </li>
+                <li className="nav-item mx-4">
+                  <Link className="nav-link" to="/Course">
+                    Course
+                  </Link>
+                </li>
+                <li className="nav-item mx-4">
+                  <Link className="nav-link" to="/Faq">
+                    FAQ
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </nav>
