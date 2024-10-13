3e1c91f01d128a9dc21b3fa143ae5de7efe7aed1
ismail ibrahim <ismailibrahimabiodun@gmail.com>
2024-10-13 04:35:21 -0700

    updating

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
         </Route>
-
+    
         {/* signIn & signUp */}
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<Signup />} />
